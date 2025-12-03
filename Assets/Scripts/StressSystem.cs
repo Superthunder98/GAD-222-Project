34a2d716c5e008fd6119ae40d8ec256df14d6e46
Superthunder98 <159098798+Superthunder98@users.noreply.github.com>
2025-12-03 10:39:02 +0800

    added post procesing

---
+++ Assets/Scripts/StressSystem.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
+
+public class StressSystem : MonoBehaviour
+{
+    [SerializeField] float stepIncreaseValue = 5f;
+
+    [Space(20)]
+
+
+    [SerializeField] GameManager gameManager;
+    [SerializeField] Volume postProcessVolume;
+    [Space(20)]
+
+    [SerializeField] int corretOrders = 0;
+    [SerializeField] int incorrectOrders = 0;
+
+    [SerializeField] float stressLevel = 0f;
+
+
+
+    private void Awake()
+    {
+        gameManager = GameObject.Find("GAMEMANAGER").GetComponent<GameManager>();
+
+        if (!gameManager)
+        {
+            Debug.LogError("GameManager not found in StressSystem");
+        }
+
+        if (!postProcessVolume)
+        {
+            Debug.LogError("PostProcessVolume not assigned in StressSystem");
+        }
+
+
+    }
+
+        void Update()
+        {
+            UpdateValues();
+            UpdateStressLevel();
+        }
+
+        void UpdateValues()
+        {
+            corretOrders = gameManager.correctOrders;
+            incorrectOrders = gameManager.incorrectOrders;
+        }
+
+        void UpdateStressLevel()
+        {
+            stressLevel = (incorrectOrders - corretOrders) * stepIncreaseValue;
+
+        postProcessVolume.weight = Mathf.Clamp((incorrectOrders - corretOrders) * stepIncreaseValue, 0f, 1f);
+        }
+
+    
+}
+
